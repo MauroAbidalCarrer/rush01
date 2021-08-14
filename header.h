79f941dbe529abf6ca741ad96d4d5a75e1026b01
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2021-08-14 18:37:33 +0200

    finished writing solve

--- header.h
+++ header.h
@@ -7,7 +7,7 @@ typedef struct game_s
 	int grid[4][4];
 	int view_points[4][4];
 		/*
-		first index corresponds to the side:0=up 1=down 2=left 3=right
+		first index corresponds to the side: 0 = up 1 = down 2 = left 3 = right
 		second index corresponds to index each side
 		*/
 } game;
@@ -37,4 +37,3 @@ void	ft_putchar(char c);
 void	ft_putstr(char *str);
 int		is_number(char c); 
 #endif
-
