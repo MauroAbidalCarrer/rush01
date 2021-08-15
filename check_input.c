a7c98186bf7a3e020e4a9949eef09d9d7eb8dc74
Kyubong CHOI <kychoi@e3r6p18.42.fr>
2021-08-15 12:49:52 +0200

    fix(all):modified for adapt to the norminette

--- check_input.c
+++ check_input.c
@@ -1,27 +1,33 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kychoi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/15 12:17:03 by kychoi            #+#    #+#             */
+/*   Updated: 2021/08/15 12:17:04 by kychoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "header.h"
+
 int	check_input(char *input)
 {
 	int		i;
 
 	i = 0;
-	/* 31 = 16 letter && 15 space */
 	if (ft_strlen(input) != 31)
 		return (0);
-	/* find each word in input string */
 	while (input[i])
 	{
-		//if pair
 		if (i % 2 == 0)
 		{
-			//if not number(48 ~ 57 in ascii) returns 0;
 			if (!is_number(input[i]))
 				return (0);
-			//if more than 4 returns 0;
 			if ((input[i] - 48) > 4 || (input[i] - 48) < 1)
 				return (0);
 		}
-		/* if impair && not space returns 0 */
 		else if (input[i] != 32)
 			return (0);
 		++i;
--- main.c
+++ main.c
@@ -1,16 +1,27 @@
-#include"header.h"
-#include<stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kychoi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/15 12:17:34 by kychoi            #+#    #+#             */
+/*   Updated: 2021/08/15 12:42:07 by kychoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int ac, char **av)
+#include "header.h"
+
+int	main(int ac, char **av)
 {
-	game g;
+	t_game	g;
 
 	if (ac != 2 || check_input(av[1]) == 0)
 	{
 		ft_putstr("Error\n");
 		return (1);
 	}
-	g = init_game(av[1]);;
+	g = init_game(av[1]);
 	if (solve(0, 0, &g) == 0)
 	{
 		ft_putstr("Error\n");
